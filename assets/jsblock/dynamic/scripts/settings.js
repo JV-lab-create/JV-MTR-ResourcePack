6c4e948bb29d527e6b0ed5827bd3d8a0d044f96f
Browndog7912 <jakevento41704@gmail.com>
2026-05-12 21:19:52 -0700

    delete uneded directory

--- assets/jsblock/dynamic/scripts/settings.js
+++
@@ -1,9 +0,0 @@
-let OPERATORS = [
-  { key: "Rikka West Rail", ch: "rwr_ch.png", en: "rwr_en.png" },
-  { key: "CrossCountry", ch: "xc_ch.png", en: "xc_en.png" },
-  { key: "Sprinter", ch: "sprinter.png", en: "sprinter.png" },
-  { key: "West Midlands Railway", ch: "west_midlands_ch.png", en: "west_midlands_en.png" },
-  { key: "Violet Line", ch: "swt_violet.png", en: "swt_violet.png" },
-  { key: "Cupid Line", ch: "p1_ch.png", en: "p1_en.png" },
-  { key: "Trident Railways", ch: "trident_ch.png", en: "trident_en.png" }
-];
